23572067b2ee7ef3ee491725edb7c9086e8100fc
haidar_aw.i <haidar_aw.i@NB21-1366>
2021-10-28 20:12:36 +0700

    change project

--- BagianB-AplikasiWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BagianB_AplikasiWeb.MvcApplication" Language="C#" %>
---
+++ BagianB-AplikasiWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BagianB_AplikasiWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
